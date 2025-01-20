7f9e20d2c83c981b7c7693459c570a47774aeedc
Zoe <zoedroulias@gmail.com>
2025-01-20 18:25:04 -0500

    add comments

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 # create a mongo client
 client = MongoClient("mongodb+srv://zdroulias:FrozenAnna0306@cluster0.h9zxq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-# connect to the database
-client.MySeriesOfFortunateEvents
-users = client.users
-events = client.events
+# get access to data
+client.MySeriesOfFortunateEvents    # connect to the database
+users = client.users                # get the users
+events = client.events              # get the events
