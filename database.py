bd36f9efebfe1f0bf063585ad755a926dd09bb8d
Zoe <zoedroulias@gmail.com>
2025-01-20 18:24:21 -0500

    create connection to the pymongo database

--- database.py
+++ database.py
@@ -1,1 +1,9 @@
-MONGODB_URI = "mongodb+srv://zdroulias:<db_password>@cluster0.h9zxq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+from pymongo import MongoClient
+
+# create a mongo client
+client = MongoClient("mongodb+srv://zdroulias:FrozenAnna0306@cluster0.h9zxq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+# connect to the database
+client.MySeriesOfFortunateEvents
+users = client.users
+events = client.events
