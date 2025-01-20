467ff55c8c9b5eec07e9b7577b1c4cd09d870918
Zoe <zoedroulias@gmail.com>
2025-01-20 18:31:37 -0500

    import werkzeug.security and update databse

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # create a mongo client
 client = MongoClient("mongodb+srv://zdroulias:FrozenAnna0306@cluster0.h9zxq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 # get access to data
-client.MySeriesOfFortunateEvents    # connect to the database
+client.my_series_of_fortunate_events    # connect to the database
 users = client.users                # get the users
 events = client.events              # get the events
 
@@ -13,7 +14,12 @@ def find_user(username: str):
     return users.find_one({"username": username})
 
 def add_user(username: str, password: str):
-    pass
+    user = {
+        "username": username,
+        "password": password
+    }
+    
+    users.insert_one(user)
 
 # EVENT FUNCTIONS
 def create_event(event_description: str, event_rating: int):
