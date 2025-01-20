ec76dda35daf00e1f138361698116f486b1515e6
Zoe <zoedroulias@gmail.com>
2025-01-20 18:28:36 -0500

    create function headers in database.py

--- database.py
+++ database.py
@@ -7,3 +7,14 @@ client = MongoClient("mongodb+srv://zdroulias:FrozenAnna0306@cluster0.h9zxq.mong
 client.MySeriesOfFortunateEvents    # connect to the database
 users = client.users                # get the users
 events = client.events              # get the events
+
+# USER FUNCTIONS
+def find_user(username: str):
+    pass
+
+def add_user(username: str, password: str):
+    pass
+
+# EVENT FUNCTIONS
+def create_event(event_description: str, event_rating: int):
+    pass
