6f4e771eb2503be7dee35ca7842d556ec3e02194
Zoe <zoedroulias@gmail.com>
2025-02-07 20:35:00 -0500

    update database.py

---
+++ utils/database.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+DB_PATH = 'data/fortunate_events.db'
+
+def create_db():
+    # create connection to and cursor for the database
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+    
+    # create tables
+    c.execute('''
+        CREATE TABLE users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL
+        )
+        
+        CREATE TABLE events (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            title TEXT NOT NULL,
+            rating INTEGER NOT NULL,
+            date TEXT NOT NULL
+        )
+    ''')
+    
+    conn.commit()
+    conn.close()
