29e0d5a7e935f40cf9c319fba635c8b999a78436
Zoe <zoedroulias@gmail.com>
2025-02-07 20:39:56 -0500

    create function for checking passwords in model.py

--- utils/model.py
+++ utils/model.py
@@ -3,5 +3,8 @@ import utils.database as db
 def find_user(username):
     return db.get_user(username)
 
+def check_password(password, hashed_password):
+    return db.check_password(password, hashed_password)
+
 def find_events(user_id):
     return db.get_events(user_id)
