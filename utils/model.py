1a000fa894fafcf269ff2b03ce9d2feb69478898
Zoe <zoedroulias@gmail.com>
2025-02-07 20:43:33 -0500

    create function to create an entry

--- utils/model.py
+++ utils/model.py
@@ -14,3 +14,6 @@ def find_events(user_id):
 
 def can_add_entry(user_id):
     return db.get_event_by_date(user_id)
+
+def add_event(user_id, title, rating):
+    db.create_event(user_id, title, rating)
