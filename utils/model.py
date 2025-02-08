fcc80fb42b4d5bc5764355c80854c6acdd3e7811
Zoe <zoedroulias@gmail.com>
2025-02-07 20:41:14 -0500

    create function in model.py to add a user to the database

--- utils/model.py
+++ utils/model.py
@@ -6,5 +6,8 @@ def find_user(username):
 def check_password(password, hashed_password):
     return db.check_password(password, hashed_password)
 
+def add_user(username, password):
+    db.create_user(username, password)
+
 def find_events(user_id):
     return db.get_events(user_id)
