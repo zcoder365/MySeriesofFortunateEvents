45983b03e2751f5f7bc65da6c6ca8661debe1af9
Zoe <zoedroulias@gmail.com>
2025-05-21 17:56:11 -0400

    update model.py

--- utils/model.py
+++ utils/model.py
@@ -10,11 +10,5 @@ def add_user(username: str, password: str):
 def get_events(username: str):
     return db.get_entries(username)
 
-def can_add_entry(user_id):
-    return db.get_event_by_date(user_id)
-
 def add_event(user_id, title, rating):
     db.create_event(user_id, title, rating)
-    
-def hash_password(password):
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
