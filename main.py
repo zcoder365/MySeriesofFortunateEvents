3fd3d43ccc89b3803a1c74de683ceb5fb66d5d17
Zoe <zoedroulias@gmail.com>
2025-05-21 17:52:38 -0400

    update imports in main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # imports
 from flask import Flask, render_template, request, redirect, session, flash, url_for
 import utils.model as model
-import utils.database as db
 
 # prep the app and add configurations
 app = Flask(__name__)
